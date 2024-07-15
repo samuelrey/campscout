33cf9e903e82d14cc936bf71f75c196b27678d30
samuel <samuelrey010@gmail.com>
2024-07-14 22:00:14 -0700

    build more CRUD operations

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from models import Campground
 import uuid
 
@@ -14,3 +14,18 @@ def add_campground(name: str):
     id = uuid.uuid4()
     campgrounds[id] = Campground(id=id, name=name)
     return {"campground": campgrounds[id]}
+    
+@app.get("/campground/{id}")
+def get_campground(id: uuid.UUID):
+    return {"campground": campgrounds[id]}
+
+@app.get("/campground")
+def get_campgrounds():
+    return {"campgrounds": campgrounds}
+
+@app.delete("/campground/{id}")
+def delete_campground(id: uuid.UUID):
+    if id not in campgrounds:
+        raise HTTPException(status_code=404, detail="Item not found.")
+    del campgrounds[id]
+    return

0a6dfe2702a9b4d90967ebf89c8b5da64fd9a83f
Sammy <samuelrey010@gmail.com>
2024-07-21 11:27:02 -0700

    add datepickers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,65 @@
 import { useState } from "react";
 import "./App.css";
 import { Box, FormControl, InputLabel, Select, MenuItem } from "@mui/material";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import dayjs from "dayjs";
+
+const today = dayjs();
+const tomorrow = dayjs().add(1, "day");
 
 function App() {
   const [age, setAge] = useState("");
+  const [startDate, setStartDate] = useState(today);
+  const [endDate, setEndDate] = useState(tomorrow);
 
   const handleChange = (event) => {
     setAge(event.target.value);
   };
 
+  const handleStartDateChange = (event) => {
+    setStartDate(event);
+  };
+
+  const handleEndDateChange = (event) => {
+    setEndDate(event);
+  };
+
   return (
     <div className="App">
       <header className="App-header" />
-      <body>
-        <Box sx={{ minWidth: 120 }}>
-          <FormControl fullWidth>
-            <InputLabel>Age</InputLabel>
-            <Select value={age} label="Age" onChange={handleChange}>
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem>
-            </Select>
-          </FormControl>
-        </Box>
-      </body>
+      <Box sx={{ minWidth: 120 }}>
+        <FormControl fullWidth>
+          <InputLabel>Age</InputLabel>
+          <Select value={age} label="Age" onChange={handleChange}>
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              label="Start Date"
+              openTo="month"
+              views={["year", "month", "day"]}
+              disablePast={true}
+              defaultValue={startDate}
+              onChange={(newDate) => {
+                handleStartDateChange(newDate);
+              }}
+            />
+            <DatePicker
+              label="End Date"
+              openTo="month"
+              views={["year", "month", "day"]}
+              disablePast={true}
+              defaultValue={endDate}
+              minDate={startDate.add(1, "day")}
+              onChange={handleEndDateChange}
+            />
+          </LocalizationProvider>
+        </FormControl>
+      </Box>
     </div>
   );
 }

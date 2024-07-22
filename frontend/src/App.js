1dbce96f586bfbfe87f207780827d756048ef8de
Sammy <samuelrey010@gmail.com>
2024-07-21 23:17:43 -0700

    progress

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import "./App.css";
 import {
-  Box,
-  FormControl,
-  InputLabel,
-  Select,
-  MenuItem,
-  Button,
+    Box,
+    FormControl,
+    InputLabel,
+    Select,
+    MenuItem,
+    Button,
 } from "@mui/material";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
@@ -17,66 +17,72 @@ const today = dayjs();
 const tomorrow = dayjs().add(1, "day");
 
 function App() {
-  const [campground, setCampground] = useState("");
-  const [startDate, setStartDate] = useState(today);
-  const [endDate, setEndDate] = useState(tomorrow);
+    const [campground, setCampground] = useState("");
+    const [startDate, setStartDate] = useState(today);
+    const [endDate, setEndDate] = useState(tomorrow);
 
-  const handleChange = (event) => {
-    setCampground(event.target.value);
-  };
+    const handleChange = (event) => {
+        setCampground(event.target.value);
+    };
 
-  const handleStartDateChange = (event) => {
-    setStartDate(event);
-  };
+    const handleStartDateChange = (event) => {
+        setStartDate(event);
+    };
 
-  const handleEndDateChange = (event) => {
-    setEndDate(event);
-  };
+    const handleEndDateChange = (event) => {
+        setEndDate(event);
+    };
 
-  return (
-    <div className="App">
-      <header className="App-header" />
-      <Box sx={{ minWidth: 120 }}>
-        <FormControl fullWidth>
-          <InputLabel>Campground</InputLabel>
-          <Select value={campground} label="Campground" onChange={handleChange}>
-            <MenuItem value={"bass-lake"}>Bass Lake</MenuItem>
-            <MenuItem value={"donner-lake"}>Donner Lake</MenuItem>
-            <MenuItem value={"dorabelle"}>Dorabelle</MenuItem>
-          </Select>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DatePicker
-              label="Start Date"
-              openTo="month"
-              views={["year", "month", "day"]}
-              disablePast={true}
-              defaultValue={startDate}
-              onChange={(newDate) => {
-                handleStartDateChange(newDate);
-              }}
-            />
-            <DatePicker
-              label="End Date"
-              openTo="month"
-              views={["year", "month", "day"]}
-              disablePast={true}
-              defaultValue={endDate}
-              minDate={startDate.add(1, "day")}
-              onChange={handleEndDateChange}
-            />
-          </LocalizationProvider>
-          <Button
-            variant={"outlined"}
-            onSubmit={alert(
-              `You will receive alerts for available campsites at ${campground} between ${startDate} and ${endDate}`
-            )}
-          >
-            Subscribe
-          </Button>
-        </FormControl>
-      </Box>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header" />
+            <Box sx={{ minWidth: 120 }}>
+                <FormControl fullWidth>
+                    <InputLabel>Campground</InputLabel>
+                    <Select
+                        value={campground}
+                        label="Campground"
+                        onChange={handleChange}
+                    >
+                        <MenuItem value={"bass-lake"}>Bass Lake</MenuItem>
+                        <MenuItem value={"donner-lake"}>Donner Lake</MenuItem>
+                        <MenuItem value={"dorabelle"}>Dorabelle</MenuItem>
+                    </Select>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DatePicker
+                            label="Start Date"
+                            openTo="month"
+                            views={["year", "month", "day"]}
+                            disablePast={true}
+                            defaultValue={startDate}
+                            onChange={(newDate) => {
+                                handleStartDateChange(newDate);
+                            }}
+                        />
+                        <DatePicker
+                            label="End Date"
+                            openTo="month"
+                            views={["year", "month", "day"]}
+                            disablePast={true}
+                            defaultValue={endDate}
+                            minDate={startDate.add(1, "day")}
+                            onChange={handleEndDateChange}
+                        />
+                    </LocalizationProvider>
+                    <Button
+                        variant={"outlined"}
+                        onClick={() => {
+                            alert(
+                                `You will receive alerts for available campsites at ${campground} between ${startDate} and ${endDate}`
+                            );
+                        }}
+                    >
+                        Subscribe
+                    </Button>
+                </FormControl>
+            </Box>
+        </div>
+    );
 }
 
 export default App;
